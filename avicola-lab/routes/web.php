99f4553ea1e806bb9efd9b76990717ad8c2c58c7
CairParavel314 <tabriz314@gmail.com>
2025-10-11 16:06:10 -0700

    Rutas.
    Actualización de rutas.

--- avicola-lab/routes/web.php
+++ avicola-lab/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GranjaController;
+use App\Http\Controllers\LoteController;
+use App\Http\Controllers\PruebaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('granjas', GranjaController::class);
+Route::resource('lotes', LoteController::class);
+Route::resource('pruebas', PruebaController::class);
