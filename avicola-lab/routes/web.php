2431182f3f376a9c60e16a0909a33755d5c34f40
CairParavel314 <tabriz314@gmail.com>
2025-10-14 06:35:25 -0700

    Rutas para reportes.

--- avicola-lab/routes/web.php
+++ avicola-lab/routes/web.php
@@ -10,3 +10,9 @@ Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('granjas', GranjaController::class);
 Route::resource('lotes', LoteController::class);
 Route::resource('pruebas', PruebaController::class);
+// Reportes
+Route::prefix('reportes')->group(function () {
+    Route::get('/dashboard-avanzado', [App\Http\Controllers\ReporteController::class, 'dashboardAvanzado'])->name('reportes.dashboard-avanzado');
+    Route::get('/comparativas-avanzadas', [App\Http\Controllers\ReporteController::class, 'comparativasAvanzadas'])->name('reportes.comparativas-avanzadas');
+});
+
